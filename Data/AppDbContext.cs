2c0a271c9244fa8f25a74b1da9197bd075b81830
nikoladomazetovikj <nikolad150@gmail.com>
2024-05-22 11:53:01 +0200

    feat: initial auth migration

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using budget_planner_api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace budget_planner_api.Data;
 
-public class AppDbContext : IdentityDbContext
+public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
     {
--- Program.cs
+++ Program.cs
@@ -84,7 +84,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
